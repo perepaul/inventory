f60880c485b2ff2f19bb7c38d27b263ab37a24e1
Lesley Ayemi <leeayemi@gmail.com>
2020-05-19 22:14:22 +0100

    finished backend design all designs are ready

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,130 @@
+@extends('home')
+
+@section('content_header')
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1> <b> Create Employee</b></h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Create Employee</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+@endsection
+
+
+@section('content')
+<section class="content">
+    <div  class="container-fluid ">
+      <div class="row">
+
+        <!-- left column -->
+        <div class="col-md-6">
+          <!-- general form elements -->
+          <div class="card card-secondary">
+            <div class="card-header">
+              <h3 class="card-title">Create Employee</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="#">Name</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Name">
+                </div>
+                <div class="form-group">
+                  <label for="#">Email Address</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email Address">
+                </div>
+               
+                <div class="form-group">
+                  <label for="#">Password</label>
+                  <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                <label for="#">Re-enter Password</label>
+                  <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Re-enter Password">
+                </div>
+                <div class="form-group">
+                  <label for="#">User Profile</label>
+                  <div class="input-group">
+                    <div class="custom-file">
+                      <input type="file" class="custom-file-input" id="exampleInputFile">
+                      <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                    </div>
+                  
+                  </div>
+                </div>
+             
+              </div>
+              <!-- /.card-body -->
+
+              <div class="card-footer">
+                <button type="submit" class="btn btn-success">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.card -->
+
+        </div>
+
+        <div class="col-md-6">
+            <form role="form">
+
+                <div class="form-group">
+                    <label for="#">Select Employee Role</label>
+                    <select class="form-control select2bs4" style="width: 100%;">
+                        <option value="Selected">Choose Role</option>
+                        <option >Admin</option>
+                        <option >Cashier</option>
+                    </select>
+                  </div>
+
+                  <div class="text-center">
+                  <div class="form-group">
+                      <label for="#">Can Create User</label>
+                      <input type="checkbox">
+                  </div>
+                  <div class="form-group">
+                      <label for="#">Can View Product</label>
+                      <input class="" type="checkbox">
+                  </div>
+                  <div class="form-group">
+                      <label for="#">Can Create Product</label>
+                      <input type="checkbox">
+                  </div>
+                  <div class="form-group">
+                      <label for="#">Can Edit Product</label>
+                      <input type="checkbox">
+                  </div>
+                  <div class="form-group">
+                      <label for="#">Can Update Stock</label>
+                      <input type="checkbox">
+                  </div>
+                  <div class="form-group">
+                      <label for="#">Can Sell Product</label>
+                      <input type="checkbox">
+                  </div>
+
+                </div>
+
+            </form>
+
+        </div>
+
+      </div>
+    </div>
+</section>
+
+    
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,81 @@
+@extends('home')
+
+@section('content_header')
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>All Employees</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Employee</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+    </section>
+      
+    
+@endsection
+
+@section('content')
+
+     <!-- Main content -->
+     <section class="content">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header text-right">
+                
+                <button class="btn btn-primary">Add Employee</button>
+                
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th>User Image</th>
+                    <th>Employee Name</th>
+                    <th>Employee Role</th>
+                    {{-- <th></th>
+                    <th>Remaining Stock</th> --}}
+                    <th>Actions</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Image Here</td>
+                    <td>
+                      Name Here
+                    </td>
+                    <td>Role Here</td>
+                    {{-- <td> Total Here</td>
+                    <td>Remaining Here</td> --}}
+                    <td>
+                      <button class="btn btn-warning">Edit <i class="fa fa-edit"></i> </button>
+                      <button class="btn btn-danger">Delete <i class="fa fa-trash"></i> </button>
+                    </td>
+                  </tr>
+                 
+                  </tbody>
+        
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+  
+          
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+    
+@endsection
